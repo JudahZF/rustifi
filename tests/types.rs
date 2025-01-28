d6e2cc69128dfd82cc4598fff499be538c7beedf
Judah Fuller <judahfuller@icloud.com>
2025-01-28 10:53:54 +0000

    Fix test (More needed)

--- tests/types.rs
+++ tests/types.rs
@@ -1,7 +1,9 @@
-use crate::types::ip::IP;
+use rustifi::types::ip::IP;
 
 #[test]
 fn test_ip() {
-    let ip = IP::new([192, 168, 1, 1]);
-    assert_eq!(ip.into(), "192.168.1.1".to_string());
+    let ip = IP::new([192, 168, 1, 1], None);
+    let with_mask = IP::new([192, 168, 1, 1], Some(24));
+    assert_eq!(format!("{}", ip), "192.168.1.1".to_string());
+    assert_eq!(format!("{}", with_mask), "192.168.1.1/24".to_string());
 }
